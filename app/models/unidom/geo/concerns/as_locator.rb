d1e626b7f9d75a19e91c0ce8b5b3f999d1409cfc
Topbit Du <topbit.du@gmail.com>
2016-09-25 11:43:16 +0800

    1, As Locator concern.

---
+++ app/models/unidom/geo/concerns/as_locator.rb
@@ -0,0 +1,25 @@
+module Unidom::Geo::Concerns::AsLocator
+
+  extend ActiveSupport::Concern
+
+  included do |includer|
+
+    has_many :locatings, class_name: 'Unidom::Geo::Locating', as:     :locator
+    has_many :locations, through:    :locatings,              source: :location
+
+=begin
+    def locate!(to: nil, by: nil, at: Time.now)
+      locatings.location_is(to).valid_at(now: at).alive.first_or_create! locator: by, opened_at: at
+    end
+
+    def locate?(to: nil, at: Time.now)
+      locatings.location_is(to).valid_at(now: at).alive.exists?
+    end
+=end
+
+  end
+
+  module ClassMethods
+  end
+
+end
