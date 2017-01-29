bc37b67070fff0e969e3f04947d8c45c7a1f2f80
Topbit Du <topbit.du@gmail.com>
2017-01-29 17:15:45 +0800

    1, Models RSpec examples manifest.

---
+++ lib/unidom/geo/models_rspec.rb
@@ -0,0 +1,2 @@
+require 'rspec/models/unidom/geo/location_spec' unless Unidom::Common::Neglection.namespace_neglected? 'Unidom::Geo::Location'
+require 'rspec/models/unidom/geo/locating_spec' unless Unidom::Common::Neglection.namespace_neglected? 'Unidom::Geo::Locating'
