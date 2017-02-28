7a96cce4356c9a596e43c817c03967df90becf28
Topbit Du <topbit.du@gmail.com>
2017-02-28 18:25:26 +0800

    1, Improve the Locating spec for the ``belongs_to :location, class_name: 'Unidom::Geo::Location'`` macro.

--- lib/rspec/models/unidom/geo/locating_spec.rb
+++ lib/rspec/models/unidom/geo/locating_spec.rb
@@ -18,6 +18,21 @@ describe Unidom::Geo::Locating, type: :model do
 
     it_behaves_like 'Unidom::Common::Concerns::ModelExtension', model_attributes
 
+    location_attributes = {
+      region_id:         SecureRandom.uuid,
+      region_type:       'Unidom::Geo::Region::Mock',
+      longitude:         120.000000,
+      latitude:          31.000000,
+      postal_address:    'Somewhere',
+      postal_code:       '812345',
+      minimum_longitude: 119.990000,
+      minimum_latitude:  30.990000,
+      maximum_longitude: 120.010000,
+      maximum_latitude:  31.010000
+    }
+
+    it_behaves_like 'belongs_to', model_attributes, :location, Unidom::Geo::Location, location_attributes
+
   end
 
 end
