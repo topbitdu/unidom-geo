b4bafe49f3525320e5c635ddde166552c7459560
Topbit Du <topbit.du@gmail.com>
2017-03-27 21:14:58 +0800

    1, Improve the As Located shared examples for the #is_located! method, & the #is_located? method.

--- lib/rspec/models/unidom/geo/concerns/as_located_shared_examples.rb
+++ lib/rspec/models/unidom/geo/concerns/as_located_shared_examples.rb
@@ -1,17 +1,26 @@
-shared_examples 'Unidom::Geo::Concerns::AsLocated' do |model_attributes|
+shared_examples 'Unidom::Geo::Concerns::AsLocated' do |model_attributes, location, locator|
 
-  locating_1_attribtues = {
-      location_id:  SecureRandom.uuid,
-      locator_id:   SecureRandom.uuid,
-      locator_type: 'Unidom::Geo::Locator::Mock'
-    }
+  context do
 
-  locating_2_attribtues = {
-      location_id:  SecureRandom.uuid,
-      locator_id:   SecureRandom.uuid,
-      locator_type: 'Unidom::Geo::Locator::Mock'
-    }
+    locating_1_attribtues = {
+        location_id:  SecureRandom.uuid,
+        locator_id:   SecureRandom.uuid,
+        locator_type: 'Unidom::Geo::Locator::Mock'
+      }
 
-  it_behaves_like 'has_many', model_attributes, :locatings, Unidom::Geo::Locating, [ locating_1_attribtues, locating_2_attribtues ]
+    locating_2_attribtues = {
+        location_id:  SecureRandom.uuid,
+        locator_id:   SecureRandom.uuid,
+        locator_type: 'Unidom::Geo::Locator::Mock'
+      }
+
+    it_behaves_like 'has_many', model_attributes, :locatings, Unidom::Geo::Locating, [ locating_1_attribtues, locating_2_attribtues ]
+
+    located = described_class.create! model_attributes
+
+    it_behaves_like 'assert_present!', located, :is_located!, [ { to: location, by: locator, at: Time.now } ], [ :to, :by, :at ]
+    it_behaves_like 'assert_present!', located, :is_located?, [ { to: location,              at: Time.now } ], [ :to,      :at ]
+
+  end
 
 end
