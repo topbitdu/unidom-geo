3cc05ac1b4cf1f3a3f7ecce9a5f5c7cd276d8c21
Topbit Du <topbit.du@gmail.com>
2017-04-09 09:40:03 +0800

    1, Improve the RSpec shared examples manifest to require the As Locator shared examples.

--- lib/unidom/geo/rspec_shared_examples.rb
+++ lib/unidom/geo/rspec_shared_examples.rb
@@ -1,2 +1,3 @@
 require 'rspec/models/unidom/geo/concerns/as_located_shared_examples'
+require 'rspec/models/unidom/geo/concerns/as_locator_shared_examples'
 require 'rspec/models/unidom/geo/concerns/as_region_shared_examples'
